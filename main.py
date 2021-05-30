1c25d5ef79ad61cc86ebf6ee06ca46e47de00844
Ken Stutzman <kenstutzman@msn.com>
2021-05-30 11:10:05 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyvantagepro import VantagePro2
+
+print("pyvantagepro")
+device = VantagePro2.from_url('tcp:192.168.1.34:22222')
+print("connected to device!")
+#print(device.gettime())
+#print("done with gettime()")
+data = device.get_current_data()
+#print(data)
+print("TempIn,HumIn,TempOut,HumOut,BarTrend,Barometer,RainDay,RainYear,WindSpeed,WindSpeed10Min,WindDir")
+print(data['TempIn'],data['HumIn'],data['TempOut'],data['HumOut'],data['BarTrend'],data['Barometer'],data['RainDay'],data['RainYear'],data['WindSpeed'],data['WindSpeed10Min'],data['WindDir'])
+#Datetime,BarTrend,Barometer,TempIn,HumIn,TempOut,WindSpeed,WindSpeed10Min,WindDir,HumOut,RainRate,UV,SolarRad,RainStorm,StormStartDate,RainDay,RainMonth,RainYear,ETDay,ETMonth,ETYear,BatteryStatus,BatteryVolts,ForecastIcon,ForecastRuleNo,SunRise,SunSet
